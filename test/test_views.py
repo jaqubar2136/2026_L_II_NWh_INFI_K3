cb2d63db02369956b26b17dcbe7772f959d0a8e3
jaqubar2137 <jaqubar2005@gmail.com>
2026-07-08 15:29:13 +0200

    Poprawka formatu JSON

--- test/test_views.py
+++ test/test_views.py
@@ -3,7 +3,7 @@ from hello_world import app
 from hello_world.formater import SUPPORTED
 
 
-class FlaskrTestCase(unittest.TestCase):
+class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         app.config['TESTING'] = True
         self.app = app.test_client()
@@ -11,8 +11,14 @@ class FlaskrTestCase(unittest.TestCase):
     def test_outputs(self):
         rv = self.app.get('/outputs')
         s = str(rv.data)
-        ','.join(SUPPORTED) in s
+        self.assertIn(
+            ', '.join(SUPPORTED), s
+        )
+
+    def test_msg_without_output(self):
+        rv = self.app.get('/')
+        self.assertEqual(b'Jakub Hello World!', rv.data)
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEqual(b'{ "imie":"Natalia", "mgs":Hello World!"}', rv.data)
+        self.assertEqual(b'{"imie":"Jakub", "msg":"Hello World!"}', rv.data)
